4343ce59ab943ffeeba83811b898f6425383a366
music-soul1-1 <72669184+music-soul1-1@users.noreply.github.com>
2024-07-04 14:25:24 +0300

    feat(docs): open swagger ui for release version too

--- NureTimetableAPI/Program.cs
+++ NureTimetableAPI/Program.cs
@@ -91,14 +91,12 @@ app.Use(async (context, next) =>
 });
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "NureTimetableAPI v1");
-    });
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "NureTimetableAPI v1");
+});
 
 app.UseHttpsRedirection();
 
