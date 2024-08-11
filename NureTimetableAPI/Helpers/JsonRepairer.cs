b7a17669e05ed0513056f787d970788f011998f3
music-soul1-1 <72669184+music-soul1-1@users.noreply.github.com>
2024-08-11 16:39:02 +0300

    feat(cist): add JsonRepairUtils lib to fix JSONs, update dependencies

---
+++ NureTimetableAPI/Helpers/JsonRepairer.cs
@@ -0,0 +1,20 @@
+﻿using JsonRepairUtils;
+
+namespace NureTimetableAPI.Helpers;
+
+public class JsonRepairer
+{
+    /// <summary>
+    /// Fixes the provided JSON.
+    /// </summary>
+    /// <param name="brokenJson"></param>
+    /// <returns></returns>
+    public static string RepairJson(string brokenJson)
+    {
+        var jsonRepair = new JsonRepair();
+
+        var repairedJson = jsonRepair.Repair(brokenJson);
+
+        return repairedJson;
+    }
+}
